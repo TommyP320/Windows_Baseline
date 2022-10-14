2f5d42318c3647588099387f2a9490c764ae01ea
TommyP320 <67606191+TommyP320@users.noreply.github.com>
2022-10-14 10:34:47 -0400

    Add files via upload

--- 02_Create_Baseline_Scripts/create_baseline_services.ps1
+++ 02_Create_Baseline_Scripts/create_baseline_services.ps1
@@ -2,4 +2,4 @@ $targets = Import-Csv ..\AllHosts.csv |
   Where-Object {$_.os -eq "Win10"} |
     Select-Object -ExpandProperty ip
 
-ICM -CN $targets -CR $creds -FilePath ..\01_Reference_Scripts\accounts.ps1 | Export-Csv .\Win10AccountsBaseline.csv
+ICM -CN $targets -CR $creds -FilePath ..\01_Reference_Scripts\services.ps1 | Export-Csv .\Win10ServicesBaseline.csv
