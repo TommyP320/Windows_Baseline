1c80aa7ed36ba1887c093f21b5b73925da7471db
TommyP320 <67606191+TommyP320@users.noreply.github.com>
2022-10-14 10:28:40 -0400

    Delete services.ps1

--- services.ps1
+++
@@ -1,5 +0,0 @@
-Get-CimInstance -ClassName Win32_Service |
-    Select-Object -Property @{n="ServiceName";e={$_.name}},
-                            @{n="Status";e={$_.state}},
-                            @{n="StartType";e={$_.stertmode}},
-                            PathName
