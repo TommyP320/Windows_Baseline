d57cea23553f73c2bf4b06a906394b689b26ea87
TommyP320 <67606191+TommyP320@users.noreply.github.com>
2022-10-14 10:28:31 -0400

    Delete processes.ps1

--- processes.ps1
+++
@@ -1,6 +0,0 @@
-Get-CimInstance -ClassName Win32_Process |
-    Select-Object -Property ProcessName,
-                            ProcessID,
-                            Path,
-                            CommandLine,
-                            @{n="Hash"; e={(Get-FileHash -Path $_.path).hash}}
